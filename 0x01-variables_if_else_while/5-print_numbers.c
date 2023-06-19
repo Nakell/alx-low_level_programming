7ebd5c334f70b8c5974ae4b8971b40be73c858f5
Nakell <mwongelijudym@gmail.com>
2023-06-19 07:09:55 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - a prigram that prints numbers of base 10
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
