4e48895c49b32ad526352cc4d3ec1731fe5ced4b
Nakell <mwongelijudym@gmail.com>
2023-06-21 02:54:13 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,6 +2,7 @@
 
 /**
  * print_alphabet_x10 - prints 10times alphabet
+ *
  * Return:void
 */
 
@@ -10,13 +11,14 @@ void print_alphabet_x10(void)
 
 	char ch;
 	int i;
+
 	i = 0;
 
 	while (i < 10)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
-		_putchar(ch)
+		_putchar(ch);
 		}
 		_putchar('\n');
 		i++;
