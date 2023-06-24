9972414fd84ffca2ede7c8b5484f18fbe7bd0962
Nakell <mwongelijudym@gmail.com>
2023-06-23 21:45:37 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for  (x = 0; x <= 9; x++)
+	{
+	if (x != 2 && x != 4)
+	_putchar('0' + x);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -5,7 +5,7 @@
  * @c: the character to print
  *
  * Return: on success 1
- * on error, -1 is returned and errno is set and errno is set appropiately
+ * on error, -1 is returned and errno is set appropiately
  */
 int _putchar(char c)
 {
