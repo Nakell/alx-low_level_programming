3fdc964735003a49b0ed20ffcae1877a4ca93253
Nakell <mwongelijudym@gmail.com>
2023-06-24 00:28:03 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,23 +3,24 @@
 
 /**
  * main - entry point
- * Description: prints the numbers from 1-100
- * but for mutliples of 3 print Fizz,for 5 print Buzz, for both print FizzBuzz
+ *
+ * Description: prints the numbers from 1-100 for mutliples of 3 print Fizz,for 5 print Buzz, for both print FizzBuzz
+ *
  * Return: 0
  */
 int main(void)
 {
-	int x;
+	int i;
 
-	for (x = 1; x <= 100; x++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (x % 15 == 0)
+		if (i % 15 == 0)
 			printf("FizzBuzz");
-		else if (x % 3 == 0)
+		else if (i % 3 == 0)
 			printf("Fizz");
 		else
-			printf("%x", x);
-		if (x < 100)
+			printf("%i", i);
+		if (i < 100)
 			printf(" ");
 	}
 	printf("\n");
