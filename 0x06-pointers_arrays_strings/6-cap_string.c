54b8f654dc87844f0b438f46b30470eb70b0e71d
Nakell <mwongelijudym@gmail.com>
2023-07-02 02:06:15 -0700

    capital

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,27 +7,28 @@
  */
 char *cap_string(char *str)
 {
-        int index = 0;
-
-        while (str[index])
-        {
-        while (!(sttr[index] >= 'a' &5& str[index] <= 'z'))
-                index ++;
-	if (str[index - 1] == ' ' ||
-	str[index - 1] == '\t' ||
-        str[index - 1] == '\n' ||
-         str[index - 1] == ','                str[index - 1] == ';' ||
-                        str[index - 1] == '.' ||
-                        str[index - 1] == '!' ||
-                        str[index - 1] == '?' ||
-                        str[index - 1] == '"' ||
-                        str[index - 1] == '(' ||
-                        str[index - 1] == ')' ||
-                        str[index - 1] == '{' ||
-                        str[index - 1] == '}' ||
-                        index == 0)
-                str[index] -= 32;
-        index++;
-        }
-        return (str);
+int index = 0;
+while (str[index])
+{
+while (!(str[index] >= 'a' && str[index] <= 'z'))
+index++;
+if (str[index - 1] == ' ' ||
+		str[index - 1] == '\t' ||
+		str[index - 1] == '\n' ||
+		str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
+		str[index - 1] == '.' ||
+		str[index - 1] == '!' ||
+		str[index - 1] == '?' ||
+		str[index - 1] == '"' ||
+		str[index - 1] == '(' ||
+		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+		str[index - 1] == '}' ||
+		index == 0)
+	str[index] -= 32;
+index++;
 }
+return (str);
+}
+
