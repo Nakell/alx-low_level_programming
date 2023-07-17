94d4c67885d83151c058817d0307f91f111206f3
Nakell <mwongelijudym@gmail.com>
2023-07-17 13:48:38 -0700

    main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: prints the name of the file
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("File :%s\n", __FILE__);
+	return (0);
+}
