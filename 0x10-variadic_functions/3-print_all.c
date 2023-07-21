e2df4c0f3eaf628a2321574d2a90b09f01b58605
Nakell <mwongelijudym@gmail.com>
2023-07-21 00:12:38 -0700

    printanyall

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -18,7 +18,7 @@ void print_all(const char * const format, ...)
 
 	va_start(arg_list, format);
 
-	while (format && format[i])
+	while (format && (format[i] != '\0'))
 	{
 		switch (format[i])
 		{
