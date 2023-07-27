c4df618a804d71a85a63421e1d009eacc588af76
Nakell <mwongelijudym@gmail.com>
2023-07-27 07:02:24 -0700

    printslist

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -10,10 +10,10 @@
  */
 int main(void)
 {
-    list_t *head;
-    list_t *new;
-    list_t hello = {"World", 5, NULL};
-    size_t n;
+	list_t *head;
+	list_t *new;
+	list_t hello = {"World", 5, NULL};
+	size_t n;
 
     head = &hello;
     new = malloc(sizeof(list_t));
