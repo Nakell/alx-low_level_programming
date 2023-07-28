30b0cf15d4b4953b9b1a5ba7daf71c5d83cb94a2
Nakell <mwongelijudym@gmail.com>
2023-07-28 09:41:00 -0700

    addspositive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,7 +7,7 @@
  * main - program adds positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return 0
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
@@ -17,9 +17,23 @@ int main(int argc, char *argv[])
 
 	if (argc > 1)
 	{
-		for (x = 1; x < argc; x+6)
+		for (x = 1; x < argc; x++)
 			z = argv[x];
 		for (y = 0; y < strlen(z); y++)
 		{
 			if (y[z] < 48 || y[z] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(z);
+		z++;
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
 
