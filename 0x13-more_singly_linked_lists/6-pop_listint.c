9ec591bad9f0df2a900e39b5959f630daf662eef
Nakell <mwongelijudym@gmail.com>
2023-08-01 12:04:50 -0700

    pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,11 +10,11 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int node;
 
-	if (*head == NULL || *head != NULL)
+	if (*head == NULL)
 		return (0);
 	node = (*head)->n;
 	temp = (*head)->next;
-	free(head);
+	free(*head);
 	*head = temp;
 	return (node);
 }
