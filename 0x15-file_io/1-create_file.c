a6050740161b2c0606423d0a2d5192676d61628e
Nakell <mwongelijudym@gmail.com>
2023-08-07 08:37:36 -0700

    createfile

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 	{
-		while (text_content[count])
+		while (text_content[count] != '\0')
 			count++;
 	}
 	result = write(filedes, text_content, count);
