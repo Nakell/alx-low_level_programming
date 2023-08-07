9703615d5f251c35ff5681f20997ac5c1325f464
Nakell <mwongelijudym@gmail.com>
2023-08-07 09:14:31 -0700

    create_files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,6 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (count = 0; text_content[count];)
-		text_content = "";
 			count++;
 	}
 	filedes = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
