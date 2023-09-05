e8aba04bb4b449689ca60b497e5b7cd2897b0307
Nakell <mwongelijudym@gmail.com>
2023-09-05 02:19:05 -0700

    cpfiles

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,7 +30,7 @@ void close_file(int fd)
 
 	if (x == -1)
 	{
-		dprintf(STDERR_FILENO, "Error; Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
