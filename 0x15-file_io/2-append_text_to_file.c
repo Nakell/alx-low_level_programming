612d1da334c4d82f963fb41feac81433d9de24c8
Nakell <mwongelijudym@gmail.com>
2023-09-04 17:21:54 -0700

    append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,7 +3,7 @@
 /**
  * append_text_to_file - appends text at the end of a file
  * @filename: the name of the file
- * @text-content: is the null terminated string
+ * @text_content: is the null terminated string
  * Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
@@ -12,17 +12,18 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	filedes = open(filename, O_APPEND | O_WRONLY)
-	if (text_content != NULL)
+	if (text_content)
 	{
-		for (count = 0; text_content[count] != '\0';)
+		for (count = 0; text_content[count];)
 			count++;
 	}
+	filedes = open(filename, O_WRONLY | O_APPEND);
 	_write = write(filedes, text_content, count);
-	if (filedes == -1)
-		return (-1);
 	if (_write == -1)
 		return (-1);
+	if (filedes == -1)
+		return (-1);
+
 	close(filedes);
 	return (1);
 }
